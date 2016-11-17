a53319ed1fe940de1514b23d1f558457cb62a244
Sami Tok <mehmetsamitok@yandex.com>
2016-11-17 23:01:49 +0300

    Inputs are redesigned as TFRecords

--- Codes/GetLSPData.py
+++ Codes/GetLSPData.py
@@ -1,42 +1,48 @@
 from LSPGlobals import FLAGS
+import LSPGlobals
 import os
-import urllib
+import urllib.request as url_request
 import sys
 import zipfile
 import glob
 import numpy as np
 from scipy.io import loadmat
-from os.path import basename as b
-from PIL import Image
-import LSPGlobals
+from scipy import misc
+import tensorflow as tf
+# from pdb import set_trace as bp
+from print_progress import print_progress
+
 
 def main():
     filename = maybe_download()
-    
+
     if not os.path.exists(os.path.join(FLAGS.data_dir, FLAGS.extracted_file)):
         extract_file(filename)
     else:
         print('Already Extracted.')
-        
+
     parse_resize_image_and_labels()
-    
+
+
 def maybe_download():
     if not os.path.exists(FLAGS.data_dir):
         os.mkdir(FLAGS.data_dir)
-    filepath = os.path.join(FLAGS.data_dir, FLAGS.comp_filename)
-    if not os.path.exists(filepath):
-        print('Downloading ', filepath, '.')
-        filepath, _ = urllib.urlretrieve(FLAGS.download_url + FLAGS.comp_filename, filepath, reporthook=dlProgress)
-        statinfo = os.stat(filepath)
-        print('Succesfully downloaded', statinfo.st_size, 'bytes.')
+    file_path = os.path.join(FLAGS.data_dir, FLAGS.comp_filename)
+    if not os.path.exists(file_path):
+        print('Downloading ', file_path, '.')
+        file_path, _ = url_request.urlretrieve(FLAGS.download_url + FLAGS.comp_filename, file_path,
+                                               reporthook=download_progress)
+        stat_info = os.stat(file_path)
+        print('Successfully downloaded', stat_info.st_size, 'bytes.')
     else:
-        print(filepath, 'already exists.')
-        
-    return filepath
+        print(file_path, 'already exists.')
 
-def dlProgress(count, blockSize, totalSize):
-    percent = int(count*blockSize*100/totalSize)
-    sys.stdout.write("\r Downloaded %d%% of %d megabytes" % (percent, totalSize/(1024*1024)))
+    return file_path
+
+
+def download_progress(count, block_size, total_size):
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write("\r Downloaded %d%% of %d megabytes" % (percent, total_size / (1024 * 1024)))
     sys.stdout.flush()
 
 
@@ -52,59 +58,92 @@ def extract_file(filename):
     finally:
         os.chdir(cwd)
         print('Done extracting')
-        
-        
+
+
 def parse_resize_image_and_labels():
     print('Resizing and packing images and labels to bin files.\n')
-    np.random.seed(1701) # to fix test set
-
-    if not os.path.exists(FLAGS.resized_dir):
-        os.mkdir(FLAGS.resized_dir)
+    np.random.seed(1701)  # to fix test set
 
     jnt_fn = FLAGS.data_dir + 'joints.mat'
-    
+
     joints = loadmat(jnt_fn)
     joints = joints['joints'].swapaxes(0, 2).swapaxes(1, 2)
     invisible_joints = joints[:, :, 2] < 0.5
     joints[invisible_joints] = 0
-    joints = joints[...,:2]
+    joints = joints[..., :2]
 
-    N_test = int(len(joints) * 0.1)
-    permTest = np.random.permutation(int(len(joints)))[:N_test].tolist()
+    image_list = np.asarray(sorted(glob.glob(FLAGS.orimage_dir + '*.jpg')))
 
-    imagelist = sorted(glob.glob(FLAGS.orimage_dir + '*.jpg'))
+    image_indexes = list(range(0, len(image_list)))
+    np.random.shuffle(image_indexes)
 
-    fp_train = open(os.path.join(FLAGS.data_dir, FLAGS.trainLabels_fn), 'w')
-    fp_test = open(os.path.join(FLAGS.data_dir, FLAGS.testLabels_fn), 'w')
-    for index, img_fn in enumerate(imagelist):
-        imgFile = Image.open(img_fn)
-        (imWidth, imHeight) = imgFile.size
-        imgFile = imgFile.resize((FLAGS.input_size,FLAGS.input_size), Image.ANTIALIAS)
-        newFileName = os.path.join(FLAGS.resized_dir, b(img_fn).replace( "jpg", "bin" ))
+    train_validation_split = int(len(image_list)*FLAGS.train_set_ratio)
+    validation_test_split = int(len(image_list)*(FLAGS.train_set_ratio+FLAGS.validation_set_ratio))
 
-        joints[index, :, 0] *= FLAGS.input_size/float(imWidth)
-        joints[index, :, 1] *= FLAGS.input_size/float(imHeight)
-        
-        im_label_pack = np.concatenate((  joints[index, :, :].reshape(LSPGlobals.TotalLabels),
-                                          np.asarray(imgFile).reshape(LSPGlobals.TotalImageBytes)  ))
-        im_label_pack.astype(np.uint8).tofile(newFileName)
+    train_indexes = np.asarray(image_indexes[:train_validation_split])
+    validation_indexes = np.asarray(image_indexes[train_validation_split:validation_test_split])
+    test_indexes = np.asarray(image_indexes[validation_test_split:])
 
-        if index in permTest:
-            print(newFileName, file=fp_test)        
-        else:
-            print(newFileName, file=fp_train)
-            
-        if (index % 100 == 0):
-            sys.stdout.write("\r%d done" % index) #"\r" deletes previous line
-            sys.stdout.flush()
-        
-        #"\r" deletes previous line
-        sys.stdout.write("\r")
-        sys.stdout.flush()
-        
+    write_to_tfrecords(image_list[train_indexes], joints[train_indexes], 'train')
+    write_to_tfrecords(image_list[validation_indexes], joints[validation_indexes], 'validation')
+    write_to_tfrecords(image_list[test_indexes], joints[test_indexes], 'test')
+
     print('Done.')
-  
-  
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _int64_feature_list(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def prepare_image(original_image_path):
+    image = misc.imread(original_image_path)
+    scaled_image = misc.imresize(image, (FLAGS.input_size, FLAGS.input_size), interp='bicubic')
+    return scaled_image, image.shape[0], image.shape[1]
+
+
+def scale_label(label, original_height, original_width):
+    label[:, 0] *= (FLAGS.input_size / float(original_width))
+    label[:, 1] *= (FLAGS.input_size / float(original_height))
+    return label.reshape(LSPGlobals.TotalLabels)
+
+
+def write_to_tfrecords(image_paths, labels, name):
+    num_examples = image_paths.shape[0]
+
+    filename = os.path.join(FLAGS.data_dir, name + '.tfrecords')
+
+    print('Writing', filename)
+    writer = tf.python_io.TFRecordWriter(filename)
+
+    for index in range(num_examples):
+        image, or_height, or_width = prepare_image(image_paths[index])  # FIXME read file
+        image_raw = image.tostring()
+
+        label = scale_label(labels[index], or_height, or_width)
+
+        features = tf.train.Features(feature={
+            'height': _int64_feature(FLAGS.input_size),
+            'width': _int64_feature(FLAGS.input_size),
+            'depth': _int64_feature(FLAGS.input_depth),
+            'label': _int64_feature_list(label.astype(int).tolist()),
+            'image_raw': _bytes_feature(image_raw)})
+
+        example = tf.train.Example(features=features)
+
+        writer.write(example.SerializeToString())
+
+        print_progress(index, num_examples-1, prefix='Progress:', suffix='Complete', bar_length=50)
+
+    writer.close()
+
 
 if __name__ == "__main__":
     main()
--- Codes/LSPGlobals.py
+++ Codes/LSPGlobals.py
@@ -7,20 +7,30 @@ tf.app.flags.DEFINE_string('download_url', 'http://www.comp.leeds.ac.uk/mat4saj/
 tf.app.flags.DEFINE_string('comp_filename', 'lspet_dataset.zip', """Name of downloaded compressed file.""")
 tf.app.flags.DEFINE_string('extracted_file', 'joints.mat', """Name of extracted joints file""")
 
-tf.app.flags.DEFINE_string('data_dir', pt.expanduser('~/Desktop/LSP_data/'), """Folder for downloading and extracting data.""")
-tf.app.flags.DEFINE_string('orimage_dir', pt.expanduser('~/Desktop/LSP_data/images/'), """Folder for saving resized train files""")
-tf.app.flags.DEFINE_string('resized_dir', pt.expanduser('~/Desktop/LSP_data/resized_images/'), """Folder for saving resized train files""")
-tf.app.flags.DEFINE_string('train_dir', pt.expanduser('~/Desktop/LSP_data/TrainData'), """Folder for saving train files""")
-tf.app.flags.DEFINE_string('input_dir', pt.expanduser('~/Desktop/LSP_data/EvalData/'),  """Input images directory.""")
-tf.app.flags.DEFINE_string('output_dir', pt.expanduser('~/Desktop/LSP_data/EvalData/Drawings'),  """Output images directory.""")
-tf.app.flags.DEFINE_string('drawing_dir', pt.expanduser('~/Desktop/LSP_data/TrainData/Drawings'), """Folder for saving train files""")
+tf.app.flags.DEFINE_string('data_dir', pt.expanduser('~/Desktop/LSP_data/'),
+                           """Folder for downloading and extracting data.""")
+tf.app.flags.DEFINE_string('orimage_dir', pt.expanduser('~/Desktop/LSP_data/images/'),
+                           """Folder for saving resized train files""")
+tf.app.flags.DEFINE_string('train_dir', pt.expanduser('~/Desktop/LSP_data/TrainData'),
+                           """Folder for saving train files""")
+tf.app.flags.DEFINE_string('input_dir', pt.expanduser('~/Desktop/LSP_data/EvalData/'),
+                           """Input images directory.""")
+tf.app.flags.DEFINE_string('output_dir', pt.expanduser('~/Desktop/LSP_data/EvalData/Drawings'),
+                           """Output images directory.""")
+tf.app.flags.DEFINE_string('drawing_dir', pt.expanduser('~/Desktop/LSP_data/TrainData/Drawings'),
+                           """Folder for saving train files""")
 
 tf.app.flags.DEFINE_string('input_type', 'jpg',  """Input type.""")
+
 
 tf.app.flags.DEFINE_string('trainLabels_fn', 'train_joints.csv', """Train labels file.""")
 tf.app.flags.DEFINE_string('testLabels_fn', 'test_joints.csv', """Test labels file.""")
 
-tf.app.flags.DEFINE_integer('input_size', 100, """One side of CNN's input image size""")
+tf.app.flags.DEFINE_integer('train_set_ratio', 0.8, """Ratio of number of train images""")
+tf.app.flags.DEFINE_integer('validation_set_ratio', 0.1, """Ratio of number of validation images""")
+tf.app.flags.DEFINE_integer('test_set_ratio', 0.1, """Ratio of number of test images""")
+
+tf.app.flags.DEFINE_integer('input_size', 220, """One side of CNN's input image size""")
 tf.app.flags.DEFINE_integer('input_depth', 3, """Color component size CNN's input image""")
 tf.app.flags.DEFINE_integer('label_count', 14, """Label count of images""")
 tf.app.flags.DEFINE_integer('label_size', 2, """Label size of images""")
@@ -40,6 +50,7 @@ TotalLabels = FLAGS.label_count * FLAGS.label_size
 
 TOWER_NAME = '-----------'
 
+
 class BodyParts:
     Right_ankle = 0
     Right_knee = 1
@@ -55,4 +66,3 @@ class BodyParts:
     Left_wrist = 11
     Neck = 12
     Head_top = 13
-    
